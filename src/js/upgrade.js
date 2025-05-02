bdd39b5bdaa7f1637a0adcaad04efe973f6bd5a0
mohdsah <mohdsah5@gmail.com>
2025-05-02 13:12:40 +0800

    Update upgrade.js

--- src/js/upgrade.js
+++ src/js/upgrade.js
@@ -1,6 +1,6 @@
 // src/js/upgrade.js
 
-import { savePlayerData } from "./firebase-game.js"; import { showToast } from "./toast.js";
+import { savePlayerData } from "./firebase-game.js"; import { showToast } from "./toast.js"; import { playerData, updateUI } from "./game-data.js";
 
-export async function upgradeBuilding(playerData, updateUI) { const cost = 500; if (playerData.money >= cost) { playerData.money -= cost; playerData.level += 1; await savePlayerData({ money: playerData.money, level: playerData.level }); showToast("Bangunan berjaya dinaik taraf!", "#4caf50"); updateUI(); } else { showToast("Tidak cukup duit untuk upgrade!", "#f44336"); } }
+export async function upgradeBuilding() { const cost = 500; if (playerData.money >= cost) { playerData.money -= cost; playerData.level += 1; await savePlayerData({ money: playerData.money, level: playerData.level }); showToast("Bangunan berjaya dinaik taraf!", "#4caf50"); updateUI(); } else { showToast("Tidak cukup duit untuk upgrade!", "#f44336"); } }
 
