e88b5ff3c39099efbce3b86e126dbc3ba37479e1
mohdsah <mohdsah5@gmail.com>
2025-05-02 13:15:48 +0800

    Update farming.js

--- src/js/farming.js
+++ src/js/farming.js
@@ -1,19 +1,6 @@
 // src/js/farming.js
 
-import { savePlayerData } from "./firebase-game.js"; import { renderQuests } from "./quests.js";
+import { playerData, updateUI } from "./game-data.js"; import { savePlayerData } from "./firebase-game.js"; import { showToast } from "./toast.js";
 
-export function addResource(playerData, type, amount) { if (playerData.inventory[type] !== undefined) { playerData.inventory[type] += amount; savePlayerData({ inventory: playerData.inventory }); renderQuests(playerData); return true; } return false; }
-
-export function upgradeBuilding(playerData) { const upgradeCost = 500; if (playerData.money >= upgradeCost) { playerData.money -= upgradeCost; playerData.level += 1; savePlayerData({ money: playerData.money, level: playerData.level }); return "Berjaya"; } else { return "Tidak cukup duit"; } }
-
-import { showToast } from "./toast.js";
-
-export function addResource(type, amount) {
-  if (playerData.inventory[type] !== undefined) {
-    playerData.inventory[type] += amount;
-    showToast(`+${amount} ${type}`, "#2196f3");
-    savePlayerData({ inventory: playerData.inventory });
-    updateUI();
-  }
-}
+export function addResource(type, amount) { if (playerData.inventory[type] !== undefined) { playerData.inventory[type] += amount; savePlayerData({ inventory: playerData.inventory }); updateUI(); showToast(+${amount} ${type}, "#2196f3"); } else { showToast("Jenis item tidak sah!", "#f44336"); } }
 
