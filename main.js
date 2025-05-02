ef58f2f0c96d115217d937e06924a35317cfedaa
mohdsah <mohdsah5@gmail.com>
2025-05-02 10:22:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -70,3 +70,12 @@ onAuthStateChanged(auth, (user) => {
     if (gamePanel) gamePanel.style.display = "none";
   }
 });
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
+}
