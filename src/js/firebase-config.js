9408b681eb107cf020e9c8d11029ae4809539e6b
mohdsah <mohdsah5@gmail.com>
2025-05-02 13:27:27 +0800

    Update firebase-config.js

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,14 +1,15 @@
+// Firebase Config
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABbbqTjJ0AQUlOVzv6SJtnjUCAWKjnQK8",
   authDomain: "dreamtowndemo.firebaseapp.com",
+  databaseURL: "https://dreamtowndemo-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "dreamtowndemo",
   storageBucket: "dreamtowndemo.firebasestorage.app",
   messagingSenderId: "388188231244",
   appId: "1:388188231244:web:c76651908a0c14f0ee06e9",
-  measurementId: "G-YY3JMP7L13",
-  databaseURL: "https://dreamtowndemo-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  measurementId: "G-YY3JMP7L13"
 };
 
 const app = initializeApp(firebaseConfig);
