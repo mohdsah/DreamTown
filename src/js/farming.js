b5fa96575fab091b1183497f265fa813b5c7bc77
mohdsah <mohdsah5@gmail.com>
2025-05-02 11:46:46 +0800

    Create farming.js

---
+++ src/js/farming.js
@@ -0,0 +1,8 @@
+// src/js/farming.js
+
+import { savePlayerData } from "./firebase-game.js"; import { renderQuests } from "./quests.js";
+
+export function addResource(playerData, type, amount) { if (playerData.inventory[type] !== undefined) { playerData.inventory[type] += amount; savePlayerData({ inventory: playerData.inventory }); renderQuests(playerData); return true; } return false; }
+
+export function upgradeBuilding(playerData) { const upgradeCost = 500; if (playerData.money >= upgradeCost) { playerData.money -= upgradeCost; playerData.level += 1; savePlayerData({ money: playerData.money, level: playerData.level }); return "Berjaya"; } else { return "Tidak cukup duit"; } }
+
